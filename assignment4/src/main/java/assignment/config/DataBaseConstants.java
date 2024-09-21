198628d57f4a9d6ccb6a5f62ee2f111fa4c002be
shreyas <120119173+Shreyas0501@users.noreply.github.com>
2024-09-21 13:59:13 +0530

    Update DataBaseConstants.java
    Removed password.

--- assignment4/src/main/java/assignment/config/DataBaseConstants.java
+++ assignment4/src/main/java/assignment/config/DataBaseConstants.java
@@ -23,7 +23,7 @@ public final class DataBaseConstants {
   public static final String FETCH_QUERY = "SELECT * FROM item";
   public static final String INSERT_QUERY = "INSERT INTO item VALUES(?, ?, ?, ?)";
 
-  public static final String PASSWORD = "Shreyas@16";
+  public static final String PASSWORD = "";
   public static final String URL = "jdbc:mysql://localhost:3306/temp";
   public static final String USER = "root";
 
